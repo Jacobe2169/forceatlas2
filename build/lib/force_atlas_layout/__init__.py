881f62b223bc2e4e674ecd9da690debaf40b9806
Jacques Fize <jacques.fize@univ-lyon2.fr>
2022-11-21 10:08:02 +0100

    delete cache files

--- build/lib/force_atlas_layout/__init__.py
+++
@@ -1,8 +0,0 @@
-from .force_atlas2 import ForceAtlas2
-from tqdm import tqdm
-
-def forceatlas2(graph,nb_iter=100,verbose = False,**parameters):
-    fa = ForceAtlas2(graph,**parameters)
-    for _ in tqdm(range(nb_iter),disable=(not verbose)):
-        fa.iteration()
-    return fa.get_positions()
