still bug on prevent overlap (probably the attraction force computation)

force_atlas_layout/energy_function.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+from numba import njit
 
 
 
@@ -25,7 +26,8 @@ def attraction(
         distributed,
     )
 
-
+    
+@njit
 def __attraction(
         node_u_coord,
         node_v_coord,
@@ -38,27 +40,27 @@ def __attraction(
         weight,
         distributed,
 ):
-    weight = 1 if not weight else weight
+
     dist = np.sqrt(
         (node_u_coord[0] - node_v_coord[0]) ** 2
         + (node_u_coord[1] - node_v_coord[1]) ** 2
     )
-    f = 1
+    f = 0
     if prevent_overlap:
         dist -= node_u_size - node_v_size
         if dist > 0:
-            f = (-scaling_ratio * weight)
-            if lin_log:
+            f = (-scaling_ratio * weight) 
+            if lin_log and dist >0:
                 f = f * math.log(1 + dist)
             if distributed:
                 f = f / node_u_mass
     else:
-        if dist > 0:
-            f = (-scaling_ratio * weight)
-            if lin_log:
-                f = f * math.log(1 + dist)
-            if distributed:
-                f = f / node_u_mass
+
+        f = (-scaling_ratio * weight)
+        if lin_log and dist >0 :
+            f = f * math.log(1 + dist)
+        if distributed:
+            f = f / node_u_mass
     return f
 
 
@@ -76,7 +78,7 @@ def repulsion(node_u, node_v, scaling_ratio, prevent_overlap=False):
     )
 
 
-
+@njit
 def __repulsion(
         node_u_coord,
         node_v_coord,
@@ -97,9 +99,9 @@ def __repulsion(
     if dist > 0:
         return scaling_ratio * node_u_mass * node_v_mass / dist / dist
     if prevent_overlap and dist < 0:
-        return 100 * scaling_ratio * node_u_mass * node_v_mass
+        return -scaling_ratio * scaling_ratio * node_u_mass * node_v_mass
 
-    return 1
+    return 0
 
 
 def repulsion_region(node_u, region, scaling_ratio, prevent_overlap=False):
@@ -114,7 +116,7 @@ def repulsion_region(node_u, region, scaling_ratio, prevent_overlap=False):
     )
 
 
-
+@njit
 def __repulsion_region(
         coord_node_u,
         mass_node_u,
@@ -127,29 +129,29 @@ def __repulsion_region(
 
     if not prevent_overlap:
         if dist > 0:
-            return scaling_ratio * (mass_node_u+1) * (mass_region+1) / dist
-        return 1
+            return scaling_ratio * (mass_node_u) * (mass_region) / np.sqrt(dist)/ np.sqrt(dist)
+        return 0
     if prevent_overlap:
         if dist > 0:
-            return scaling_ratio * (mass_node_u+1) * (mass_region+1) / dist
+            return scaling_ratio * (mass_node_u) * (mass_region) /  np.sqrt(dist) / np.sqrt(dist)
         elif dist < 0:
-            return -scaling_ratio * (mass_node_u+1) * (mass_region+1) / np.sqrt(dist)
+            return 100 * (mass_node_u) * (mass_region) / dist
 
-    return 1
+    return 0
 
 
 def gravity(node_u, gravity, scaling_ratio, strong_gravity=False):
     return __gravity(node_u.coord, node_u.mass, scaling_ratio, gravity, strong_gravity)
 
 
-
+@njit
 def __gravity(node_u_coord, node_u_mass, scaling_ratio, gravity, strong_gravity: bool):
     dist = np.sqrt(node_u_coord[0] ** 2 + node_u_coord[1] ** 2)
     if dist > 0:
         if strong_gravity:
-            return scaling_ratio * gravity * (node_u_mass+1)
-        return (gravity * (node_u_mass+1) * scaling_ratio)/dist
-    return 1
+            return scaling_ratio * gravity * (node_u_mass)
+        return (gravity * (node_u_mass) * scaling_ratio)/dist
+    return 0
 
 
 
@@ -173,15 +175,14 @@ def adjust_speed(speed,nodes_attributes,jitter_tolerance=1.0,speed_efficiency=1.
                                     graph_size**2)))
 
     
-    if total_effective_traction and total_swinging / total_effective_traction > 2.0:
+    if total_swinging / total_effective_traction > 2.0:
         if speed_efficiency > min_speed_efficiency:
             speed_efficiency *= .5
         jt = max(jt, jitter_tolerance)
 
     
-    targetSpeed = float('inf')
-    if total_swinging != 0:
-        targetSpeed = jt * speed_efficiency * total_effective_traction / total_swinging
+
+    targetSpeed = jt * speed_efficiency * total_effective_traction / total_swinging
 
     if total_swinging > jt * total_effective_traction:
         if speed_efficiency > min_speed_efficiency:
@@ -191,3 +192,5 @@ def adjust_speed(speed,nodes_attributes,jitter_tolerance=1.0,speed_efficiency=1.
 
     speed += min(targetSpeed - speed, .5 * speed)
     return speed
+
+

force_atlas_layout/force_atlas2.py
@@ -6,7 +6,8 @@ from .node import NodeCollection, Node
 # from .utils import *
 from .region import RootRegion
 from .energy_function import attraction, repulsion, gravity,adjust_speed
-from joblib import Parallel,delayed
+import random
+from typing import Optional
 
 
 
@@ -24,11 +25,11 @@ class ForceAtlas2(object):
         scaling_ratio: float = 2.0,
         gravity: float = 1.0,
         speed: float = 1.0,
-        speed_efficiency: float =1.0,
-        jitter_tolerance: float=1.0,
-        max_force:float = 20.,
+        speed_efficiency: float = 1.0,
+        jitter_tolerance: float = 1.0,
+        max_force:Optional[float] = None,
         quadtree_maxsize:int = 1000,
-        outbound_attraction_distribution: bool = False,
+        dissuade_hubs: bool = False,
         prevent_overlap: bool = True,
         barnes_hut_optimize: bool = False,
         barnes_hut_theta: float = 1.2,
@@ -84,7 +85,7 @@ class ForceAtlas2(object):
         self.barnes_hut_theta = barnes_hut_theta
         self.barnes_hut_optimize = barnes_hut_optimize
         self.prevent_overlap = prevent_overlap
-        self.outbound_attraction_distribution = outbound_attraction_distribution
+        self.dissuade_hubs = dissuade_hubs
         self.speed = speed
         self.gravity = gravity
         self.scaling_ratio = scaling_ratio
@@ -120,9 +121,9 @@ class ForceAtlas2(object):
                 "id": node,
                 "dx": 0,
                 "dy": 0,
-                "mass": 1 + self.graph.degree(node),
-                "x": np.random.rand()*100 if not node in positions else positions[node][0],
-                "y": np.random.rand()*100 if not node in positions else positions[node][1],
+                "mass": 1+self.graph.degree(node),
+                "x": random.random()*1000 if not node in positions else positions[node][0],
+                "y": random.random()*1000 if not node in positions else positions[node][1],
                 "size": self.graph.degree(node) if not node in sizes else sizes[node],
             }
             self.nodes_attributes + Node(**param)
@@ -134,6 +135,8 @@ class ForceAtlas2(object):
             edges_weights = list(nx.get_edge_attributes(self.graph, "weight").values())
             self.weight_min = np.min(edges_weights)
             self.weight_max = np.max(edges_weights)
+        
+
 
     def get_positions(self):
         """
@@ -161,7 +164,7 @@ class ForceAtlas2(object):
         # Update nodes attributes by storing previous state information and reinitialize
         # node mass
         for node in self.graph:
-            self.nodes_attributes[node].mass = 1 + self.graph.degree(node)
+            self.nodes_attributes[node].mass = 1+self.graph.degree(node)
             self.nodes_attributes[node].old_dx = self.nodes_attributes[node].dx
             self.nodes_attributes[node].old_dy = self.nodes_attributes[node].dy
 
@@ -179,17 +182,13 @@ class ForceAtlas2(object):
 
         # If outbound_attraction_distribution active, compensate
         outbound_compensation = 0
-        if self.outbound_attraction_distribution:
+        if self.dissuade_hubs:
             for n in self.graph:
                 outbound_compensation += self.nodes_attributes[n].mass
 
             outbound_compensation /= len(self.graph)
 
-        def foo(nodes):
-            for n in nodes:
-                self.root_region.apply_force(
-                    n, self.barnes_hut_theta, self.scaling_ratio, self.prevent_overlap
-                )
+
         # Apply Repulsion
         if self.barnes_hut_optimize:
             for n in self.graph:
@@ -198,12 +197,14 @@ class ForceAtlas2(object):
                 )
         else:
             for n1 in self.graph:
+                nu = self.nodes_attributes[n1]
                 for n2 in self.graph:
+                    nv = self.nodes_attributes[n2]
                     if n1 == n2:
                         continue
                     factor = repulsion(
-                        self.nodes_attributes[n1],
-                        self.nodes_attributes[n2],
+                        nu,
+                        nv,
                         self.scaling_ratio,
                         prevent_overlap=self.prevent_overlap,
                     )
@@ -213,39 +214,24 @@ class ForceAtlas2(object):
         for n in self.graph:
             factor = gravity(self.nodes_attributes[n], gravity=self.gravity,scaling_ratio=self.scaling_ratio,strong_gravity=self.strong_gravity_mode)
             self.nodes_attributes.apply_g(n, factor)
-       
-
     
         for src, tar, attr in self.graph.edges(data=True):  # type: ignore
             w = 1
             if self.edge_weight_influence > 0:
-                w = attr["weight"]
+                w = attr["weight"] ** self.edge_weight_influence
                 if self.normalize_edge_weights:
                     w = (w - self.weight_min) / (self.weight_max - self.weight_min)
 
             factor = attraction(
                 self.nodes_attributes[src],
                 self.nodes_attributes[tar],
-                outbound_compensation if self.outbound_attraction_distribution else 1,
+                outbound_compensation if self.dissuade_hubs else 1,
                 lin_log=self.lin_log_mode,
                 prevent_overlap=self.prevent_overlap,
-                weight=w,distributed=self.outbound_attraction_distribution
+                weight=w,distributed=self.dissuade_hubs
             )
             self.nodes_attributes.apply(src, tar, factor)
 
-        """
-        force = Math.sqrt(
-          Math.pow(NodeMatrix[n + NODE_DX], 2) +
-            Math.pow(NodeMatrix[n + NODE_DY], 2)
-        );
-
-        if (force > MAX_FORCE) {
-          NodeMatrix[n + NODE_DX] =
-            (NodeMatrix[n + NODE_DX] * MAX_FORCE) / force;
-          NodeMatrix[n + NODE_DY] =
-            (NodeMatrix[n + NODE_DY] * MAX_FORCE) / force;
-        }
-        """
         
         # Adjust speed and apply changes to nodes' position        
         self.speed = adjust_speed(self.speed,self.nodes_attributes,self.jitter_tolerance,self.speed_efficiency)
@@ -254,18 +240,19 @@ class ForceAtlas2(object):
             ni = self.nodes_attributes[n]
             
             force = np.sqrt(ni.dx**2 + ni.dy**2)
-            if force > self.max_force:
+            if self.max_force and force > self.max_force :
                 self.nodes_attributes[n].dx = (self.nodes_attributes[n].dx*self.max_force)/force
                 self.nodes_attributes[n].dy = (self.nodes_attributes[n].dy*self.max_force)/force
 
 
-            swinging = np.sqrt((ni.old_dx - ni.dx) **2 + (ni.old_dy - ni.dy)**2)
+            swinging = ni.mass* np.sqrt((ni.old_dx - ni.dx) **2 + (ni.old_dy - ni.dy)**2)
+                
             if self.prevent_overlap:
-                node_speed= self.speed/(1+np.sqrt(self.speed*swinging))
-            else:
-                node_speed = 0.1 * self.speed / (1.0 + np.sqrt(self.speed * swinging))
+                node_speed = 0.1 * self.speed / (1.0 + np.sqrt(self.speed *swinging))
                 df = np.sqrt(ni.dx**2 + ni.dy**2)
-                node_speed = min(node_speed*df,10)/df
+                node_speed = min(node_speed*df,10.)/df
+            else:
+                node_speed= self.speed/(1+np.sqrt(self.speed*swinging))
 
 
             self.nodes_attributes[n].x = (

force_atlas_layout/region.py
@@ -37,7 +37,7 @@ class RootRegion():
             for n in self.nodes:
                 dist = np.sqrt((self.nodes_attributes[n].x - self.massCenterX) ** 2 + (
                             self.nodes_attributes[n].y - self.massCenterY) ** 2)
-                self.size = np.max([self.size, 2 * dist])
+                self.size = max(self.size, 2 * dist)
 
 
 
@@ -51,12 +51,12 @@ class RootRegion():
 
             for n in self.nodes:
                 if self.nodes_attributes[n].x < self.massCenterX:
-                    if self.nodes_attributes[n].y > self.massCenterY:
+                    if self.nodes_attributes[n].y < self.massCenterY:
                         top_left_nodes.append(n)
                     else:
                         bottom_left_nodes.append(n)
                 else:
-                    if self.nodes_attributes[n].y > self.massCenterY:
+                    if self.nodes_attributes[n].y < self.massCenterY:
                         top_right_nodes.append(n)
                     else:
                         bottom_right_nodes.append(n)
@@ -78,7 +78,7 @@ class RootRegion():
 
     def apply_force(self, n, barnes_hut_theta,scaling_ratio,prevent_overlap):
         nu = self.nodes_attributes[n]
-        if len(self.nodes) < 2:
+        if len(self.nodes) == 1 and len(self.subregions) >0:
             ni = self.nodes_attributes[self.nodes[0]]
             factor = repulsion(nu, ni, scaling_ratio=scaling_ratio, prevent_overlap=prevent_overlap)
             self.nodes_attributes.apply_r(n,self.subregions[0],factor)